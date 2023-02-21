49b125319374eda39af69c97bfe778f6c051b2e4
krhistopher <krhistopher@weon.com.br>
2023-02-20 22:20:24 -0300

    falta finalizar

---
+++ src/components/cardHome.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="backgroundCard">
+        <h1> Esse é o componente pai </h1>
+        <div class="card">
+            <svg-icon type="mdi" :path="path"></svg-icon>
+            <dadosCardHome class="cardTitulo" :CardTitulo="cardData[0].titulo"/>
+            <dadosCardHome class="cardSubtitulo" :CardSubtitulo="cardData[0].subtitulo"/>
+        </div>
+
+    </div>
+    
+</template>
+
+<script>
+import dadosCardHome from './dadosCardHome.vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiWalletOutline } from '@mdi/js';
+export default {
+    components: {
+        dadosCardHome,
+        SvgIcon
+    },
+    data(){
+        return {
+            path: mdiWalletOutline,
+            cardData: [
+                {
+                    titulo: "$143,624",
+                    subtitulo: "Your Bank Balance"
+
+                }
+            ],
+            
+        }
+    }
+    
+    
+}
+</script>
+
+<style scoped>
+
+#backgroundCard {
+    width: auto;
+    height: auto;
+    background-color: #f9fafc;
+    padding-bottom: 20vh;
+}
+
+.card {
+    border: 0px solid black;
+    margin-bottom: 1%;
+    margin-left: 1%;
+    padding: 3% 0% 3% 0%;
+    border-radius: 20px;
+    width: 25vh;
+    background-color: #ffffff;
+    box-shadow: -7px 10px 25px #d5d7e3;
+}
+
+dadosCardHome {
+    display: flex;
+    flex-direction: column;
+}
+
+</style>
---
+++ src/components/dadosCardHome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ CardTitulo }}
+        {{ CardSubtitulo }}
+
+    </div>
+    
+</template>
+
+<script>
+export default {
+    props: ['CardIcone','CardTitulo','CardSubtitulo']
+    
+}
+</script>
+
+<style scoped>
+
+</style>
