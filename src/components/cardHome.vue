94c479c5193a7cfa112b10c344ba2f1a3f034e7e
krhistopher <krhistopher@weon.com.br>
2023-02-28 14:25:15 -0300

    Aprendendo responsividade

--- src/components/cardHome.vue
+++ src/components/cardHome.vue
@@ -1,98 +1,594 @@
 <template>
-    <div>
-        <div id="backgroundCard">
-            <div class="card">
-                <svg-icon type="mdi" :path="path1" class="cardIcon"></svg-icon>
-                <dadosCardHome class="cardTitulo" :CardTitulo="cardData[0].titulo"/>
-                <dadosCardHome class="cardSubtitulo" :CardSubtitulo="cardData[0].subtitulo"/>
-            </div>
-            <div class="card">
-                <svg-icon type="mdi" :path="path2" class="cardIcon"></svg-icon>
-                <dadosCardHome class="cardTitulo" :CardTitulo="cardData[1].titulo"/>
-                <dadosCardHome class="cardSubtitulo" :CardSubtitulo="cardData[1].subtitulo"/>
-            </div>
-
-        </div>
-    </div>    
+  <div>
+    <h2> My Card List </h2>
+    <div id="backgroundCard">
+      <div class="card" v-for="savingAllCards in cardData" :key="savingAllCards.key">
+        <svg-icon type="mdi" :path="savingAllCards.path" class="cardIcon"></svg-icon>
+        <dadosCardHome class="cardTitulo" :CardTitulo="savingAllCards.titulo"/>
+        <dadosCardHome class="cardSubtitulo" :CardSubtitulo="savingAllCards.subtitulo"/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import dadosCardHome from './dadosCardHome.vue';
-import SvgIcon from '@jamescoyle/vue-icon';
-import { mdiWalletOutline } from '@mdi/js';
-import { mdiCalendarToday } from '@mdi/js';
+import dadosCardHome from "./dadosCardHome.vue";
+import SvgIcon from "@jamescoyle/vue-icon";
+import { mdiWalletOutline } from "@mdi/js";
+import { mdiCalendarToday } from "@mdi/js";
 export default {
-    components: {
-        dadosCardHome,
-        SvgIcon
-    },
-    data(){
-        return {
-            path1: mdiWalletOutline,
-            path2: mdiCalendarToday,
-            
-            cardData: [
-                {
-                    titulo: "$143,624",
-                    subtitulo: "Your Bank Balance"
-
-                },
-                {
-                    titulo: "7",
-                    subtitulo: "Employees working today"
-
-                }
-            ],
-            
-        }
-    }
-    
-    
-}
+  components: {
+    dadosCardHome,
+    SvgIcon,
+  },
+  data() {
+    return {
+    //   path: [mdiWalletOutline, mdiCalendarToday],
+      cardData: [
+        {
+          path: mdiWalletOutline,
+          titulo: "$143,624",
+          subtitulo: "Your Bank Balance",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "22",
+          subtitulo: "Teste 20",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7",
+          subtitulo: "Employees working today",
+        },
+        {
+          path: mdiCalendarToday,
+          titulo: "7viagem",
+          subtitulo: "Employees working today",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300;500&display=swap");
 
-@import url('https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300;500&display=swap');
+div h2{
+    align-items: center;
+}
 
 #backgroundCard {
-    width: auto;
-    height: auto;
-    background-color: #f9fafc;
-    padding-bottom: 20vh;
-    display: flex;
-    flex-direction: row;
+  max-width: 1920px;
+  min-width: 320px;
+  max-height: 1080px;
+  min-height: 568px;
+  background-color: #f9fafc;
+  padding-bottom: 20vh;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 1%;
 }
 
 .card {
-    border: 0px solid black;
-    margin-bottom: 1%;
-    margin-left: 1%;
-    padding: 1% 0% 2% 1%;
-    border-radius: 20px;
-    width: 25vh;
-    background-color: #ffffff;
-    box-shadow: -7px 10px 25px #d5d7e3;
+  border: 0px solid black;
+  margin-bottom: 1%;
+  margin-left: 1%;
+  padding: 1% 0% 2% 1%;
+  border-radius: 20px;
+  min-width: 150px;
+  min-height: 150px;
+  background-color: #ffffff;
+  box-shadow: -7px 10px 25px #77777786;
 }
 
 .card dadosCardHome {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
 
 .cardTitulo {
-    font-weight: bold;
-    font-size: larger;
-    font-family: 'Signika Negative', sans-serif;
-    margin-bottom: 3%;
+  font-weight: bold;
+  font-size: larger;
+  font-family: "Signika Negative", sans-serif;
+  margin-bottom: 3%;
 }
 
 .cardSubtitulo {
-    font-family: 'Signika Negative', sans-serif;
-    font-size: x-small;
-    width: 10vh;
-    word-wrap: break-word;
-
+  font-family: "Signika Negative", sans-serif;
+  font-size: x-small;
+  max-width: 50px;
+  word-wrap: break-word;
 }
-
 </style>
